85ded50eb5380c30a490d4391d5042cccf94b9dc
Lewis Hay <29063729+lewishay@users.noreply.github.com>
2020-12-23 15:55:25 +0000

    Added metronome audio files and basic javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,10 @@
-function playNote(note) {
-  var audio = document.getElementById("note-" + note);
-  var selectedButton = document.getElementById("button-" + note);
+const pageAudio = document.getElementsByTagName("audio");
+const pageButtons = document.getElementsByClassName("button");
+
+function globalAudioControl(audio, selectedButton) {
   if(audio.paused) {
-    var sounds = document.getElementsByTagName("audio");
-    for(i = 0; i < sounds.length; i++) sounds[i].pause();
-    var buttons = document.getElementsByClassName("note-button");
-    for(i = 0; i < buttons.length; i++) buttons[i].classList.remove("select");
+    for(i = 0; i < pageAudio.length; i++) pageAudio[i].pause();
+    for(i = 0; i < pageButtons.length; i++) pageButtons[i].classList.remove("select");
     selectedButton.classList.add("select");
     audio.currentTime = 0;
     audio.play();
@@ -15,7 +14,30 @@ function playNote(note) {
   }
 }
 
-var slider = document.getElementById("bpm-slider");
-slider.oninput = function() {
-  document.getElementById("current-bpm").innerHTML = this.value;
-} 
+function playNote(note) {
+  const audio = document.getElementById("note-" + note);
+  const selectedButton = document.getElementById("button-" + note);
+  globalAudioControl(audio, selectedButton)
+}
+
+const metronomeSlider = document.getElementById("bpm-slider");
+const metronomeAudio = document.getElementById("metronome");
+const metronomeButton = document.getElementById("metronome-button")
+
+function sliderUpdate() {
+  document.getElementById("current-bpm").innerHTML = metronomeSlider.value;
+  if(!metronomeAudio.paused) {
+    // code here to adjust playback based on BPM
+  }
+}
+
+metronomeSlider.oninput = sliderUpdate;
+
+function metronomeAction() {
+  globalAudioControl(metronomeAudio, metronomeButton)
+  sliderUpdate();
+}
+
+window.onload = function() {
+  document.getElementById("current-bpm").innerHTML = metronomeSlider.value;
+}
